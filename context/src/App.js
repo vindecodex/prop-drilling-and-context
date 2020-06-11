762edbaceac24dc89f79a18e1821d8bc76403768
Vincent Villaluna <villalunavincent@gmail.com>
2020-06-11 12:42:55 +0800

    completed context implementation

--- context/src/App.js
+++ context/src/App.js
@@ -1,3 +1,15 @@
-export default App = () => {
-  return <h1>Hello, World</h1>;
+import React from 'react';
+import Form from './components/Form'
+import Provider from './provider.js'
+
+const App = () => {
+	return (
+		<>
+			<Provider>
+				<Form />
+			</Provider>
+		</>
+	)
 };
+
+export default App;
