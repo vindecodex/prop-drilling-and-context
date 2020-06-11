df26f287b89a98985271f094a32270bb464fff00
Vincent Villaluna <villalunavincent@gmail.com>
2020-06-11 12:41:50 +0800

    created Form component

---
+++ context/src/components/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Input from './Input';
+import Button from './Button';
+import Output from './Output';
+
+const Form = () => {
+	return(
+		<>
+			<Input />
+			<Button />
+			<Output />
+		</>
+	)
+}
+
+export default Form;
